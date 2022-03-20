35c3e049587f915df63dafcda7d5eeab0b6fe79c
Patrik PUCELJ <patrik.pucelj@gmail.com>
2022-03-20 15:10:02 +0100

    Project

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css">
+        <title>Accueil</title>
+</head>
+
+<body class="corps">
+    <header>
+        <h1 id="logo">CVBank</h1>
+    </header>
+    <nav>
+            <a href="index.php">Accueil</a>
+            <a href="creationOffre.php">Créer une offre</a>
+            <a href="voirOffre.php">Voir les offres</a>
+            <a href="connexion.php">Connexion</a>
+    </nav>
+    <main>
+        <h1>Bonjour</h1>
+    </main>
+    <footer>
+        <p>Références</p>
+    </footer>
+</body>
